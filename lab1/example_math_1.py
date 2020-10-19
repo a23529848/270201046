9e0929fb387ce21516646927da3a78881d43fb3f
a23529848 <berkeudunman@std.iyte.edu.tr>
2020-10-19 09:19:03 +0000

    paranthesis are important

---
+++ lab1/example_math_1.py
@@ -0,0 +1,11 @@
+print("2x**2+6x-20 is your function")
+a=2;
+b=6;
+c=-20;
+
+find_roots_1=(-b+((b**2-4*a*c)**0.5))/(2*a)
+find_roots_2=(-b-((b**2-4*a*c)**0.5))/(2*a)
+
+
+print("Root 1=",find_roots_1)
+print("Root 2=",find_roots_2)
