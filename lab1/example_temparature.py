e7b9f2dcb56eb6aa52ae625f4cc30cce90bffcdf
a23529848 <berkeudunman@std.iyte.edu.tr>
2020-10-19 09:26:36 +0000

    temparature

---
+++ lab1/example_temparature.py
@@ -0,0 +1,5 @@
+celcius_input=float(input("Please write your temparature in celcius="))
+
+fahrenheit_output=celcius_input*1.8 + 32;
+
+print("This is your fahrenheit temparature=",fahrenheit_output)
