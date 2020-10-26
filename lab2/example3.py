6b481f9f80efb730e8ab12253053f3fd76ea6d0e
a23529848 <berkeudunman@std.iyte.edu.tr>
2020-10-26 09:28:18 +0000

    example3

--- lab2/example3.py
+++ lab2/example3.py
@@ -1,10 +1,10 @@
 normal_price=3
 
-age=int(input("Please print your age"))
+age=int(input("Please print your age:"))
 
-if age > 60 and age < 6 :
-  normal_price="Free"
-elif age<18 :
+if age > 60 or age <6 :
+  normal_price=0
+elif age<18  :
   normal_price=normal_price-normal_price*(50/100)
 
 print("You can enter with",normal_price)
