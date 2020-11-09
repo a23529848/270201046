13398b6e056ed2309f2bd6e46f3f0a3b8c1cd476
a23529848 <berkeudunman@std.iyte.edu.tr>
2020-11-09 09:18:42 +0000

    ex4

--- lab3/example3.py
+++ lab3/example3.py
@@ -1,7 +1,7 @@
 list=[8,60,43,55,25,134,1]
 sumOfThem=0
 for elements in range(len(list)):
-  print(list[elements])
+  print(f"adding element..:{list[elements]}")
   sumOfThem=int(list[elements])+sumOfThem
-  print(sumOfThem)
+  print("Total="+str(sumOfThem))
   print("*")
---
+++ lab3/example4.py
@@ -0,0 +1,8 @@
+a=int(input("Number:"))
+b=int(input("Power:"))
+
+mult=1
+
+for x in range(1,b+1):
+  mult=a*mult
+  print(mult)
